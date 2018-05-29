856db8043a89d018428de0760099c88958289b72
at-nghitran <nghi.tran@asiantech.vn>
2018-05-30 01:07:51 +0700

    update route url

--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,8 @@ var groupController = require('../controllers/groups');
 router.post('/', userController.createNewUser);
 router.get('/', userController.getListUsers);
 router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
+router.put('/:id/edit', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.get('/:name/:phone', userController.findUserByNameAndPhone);
-
-/* Api Group */
-router.post('/', groupController.createNewGroup);
-router.get('/', groupController.getListGroup);
-router.get('/:id', groupController.getGroupById);
-router.put('/:id', groupController.updateGroup);
-router.delete('/:id', groupController.deleteGroup);
 
 module.exports = router;
